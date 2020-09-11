f411d2249febfa820447aeb80a60a97a78e7cab1
RiajulpHero <67540508+RiajulpHero@users.noreply.github.com>
2020-09-11 19:25:42 +0600

    photos-album-complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [element, setElement] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/photos')
+    .then(res => res.json())
+    .then(data => {
+      data = data.slice(0, 20);
+      setElement(data);
+    });
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {
+       element.map(ph => <Photos element={ph}></Photos>)
+     }
     </div>
   );
+}
+function Photos(props) {
+  return(
+    <div>
+      <img src={props.element.url} alt={props.element.thumbnailUrl}/>
+      <h3>{props.element.title}</h3>
+    </div>
+  )
 }
 
 export default App;
